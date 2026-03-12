c324317243d23a35d904f4822c552fd7518942cf
igorsalesleallima <igorsalesleallima@gmail.com>
2026-03-12 16:27:55 -0300

    atevidade
    algumas melhorias

--- UnimaxOS/backend.py
+++ UnimaxOS/backend.py
@@ -1,17 +1,64 @@
-
 import os
 import json
-from flask import Flask, render_template, request, redirect, url_for
+import smtplib
+from email.mime.text import MIMEText
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = "chave_super_secreta"
 
 ARQUIVO_JSON = "ocorrencias.json"
+ARQUIVO_USUARIOS = "usuarios.json"
+
 UPLOAD_FOLDER = "static/uploads"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-# Cria pasta uploads se não existir
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
+
+
+# =========================
+# FUNÇÕES AUXILIARES
+# =========================
+
+def carregar_ocorrencias():
+
+    if not os.path.exists(ARQUIVO_JSON):
+        return []
+
+    try:
+        with open(ARQUIVO_JSON, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except:
+        return []
+
+
+def salvar_ocorrencias(dados):
+
+    with open(ARQUIVO_JSON, "w", encoding="utf-8") as f:
+        json.dump(dados, f, indent=4, ensure_ascii=False)
+
+
+def gerar_novo_id(dados):
+
+    if not dados:
+        return "1"
+
+    ultimo_id = max(int(o["id"]) for o in dados)
+    return str(ultimo_id + 1)
+
+
+def carregar_usuarios():
+
+    if not os.path.exists(ARQUIVO_USUARIOS):
+        return []
+
+    try:
+        with open(ARQUIVO_USUARIOS, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except:
+        return []
+
 
 # =========================
 # ROTAS PRINCIPAIS
@@ -28,39 +75,88 @@ def home():
 
 
 # =========================
-# FUNÇÕES AUXILIARES
+# LOGIN
 # =========================
 
-def carregar_ocorrencias():
-    if not os.path.exists(ARQUIVO_JSON):
-        return []
+@app.route("/login", methods=["GET", "POST"])
+def login():
 
-    try:
-        with open(ARQUIVO_JSON, "r", encoding="utf-8") as f:
-            return json.load(f)
-    except:
-        return []
+    if request.method == "POST":
 
+        email = request.form.get("email")
+        senha = request.form.get("senha")
 
-def salvar_ocorrencias(dados):
-    with open(ARQUIVO_JSON, "w", encoding="utf-8") as f:
-        json.dump(dados, f, indent=4, ensure_ascii=False)
+        usuarios = carregar_usuarios()
 
+        for u in usuarios:
+            if u["email"] == email and u["senha"] == senha:
 
-def gerar_novo_id(dados):
-    if not dados:
-        return "1"
+                session["usuario"] = u["nome"]
+                session["email"] = u["email"]
 
-    ultimo_id = max(int(o["id"]) for o in dados)
-    return str(ultimo_id + 1)
+                return redirect(url_for("abrir_ocorrencia"))
+
+        return "Email ou senha inválidos"
+
+    return render_template("login.html")
 
 
 # =========================
-# A) MÓDULO OPERADOR
+# LOGOUT
+# =========================
+
+@app.route("/logout")
+def logout():
+
+    session.clear()
+    return redirect(url_for("login"))
+
+
+# =========================
+# CADASTRO
+# =========================
+
+@app.route("/cadastro")
+def cadastro():
+    return render_template("cadastro.html")
+
+
+@app.route("/cadastrar", methods=["POST"])
+def cadastrar():
+
+    nome = request.form.get("nome")
+    email = request.form.get("email")
+    senha = request.form.get("senha")
+
+    usuarios = carregar_usuarios()
+
+    for u in usuarios:
+        if u["email"] == email:
+            return "Este email já está cadastrado"
+
+    novo_usuario = {
+        "nome": nome,
+        "email": email,
+        "senha": senha
+    }
+
+    usuarios.append(novo_usuario)
+
+    with open(ARQUIVO_USUARIOS, "w", encoding="utf-8") as f:
+        json.dump(usuarios, f, indent=4)
+
+    return redirect(url_for("login"))
+
+
+# =========================
+# ABRIR OCORRÊNCIA
 # =========================
 
 @app.route("/abrir", methods=["GET", "POST"])
 def abrir_ocorrencia():
+
+    if "usuario" not in session:
+        return redirect(url_for("login"))
 
     if request.method == "POST":
 
@@ -68,7 +164,6 @@ def abrir_ocorrencia():
         novo_id = gerar_novo_id(dados)
 
         setor = request.form.get("setor")
-        operador = request.form.get("operador")
         descricao = request.form.get("descricao")
 
         foto = request.files.get("foto")
@@ -83,7 +178,8 @@ def abrir_ocorrencia():
         nova_ocorrencia = {
             "id": novo_id,
             "setor": setor,
-            "operador": operador,
+            "operador": session["usuario"],
+            "email": session["email"],
             "descricao": descricao,
             "foto": caminho_foto,
             "status": "Aberto",
@@ -94,13 +190,121 @@ def abrir_ocorrencia():
         dados.append(nova_ocorrencia)
         salvar_ocorrencias(dados)
 
+        enviar_email_nova_ocorrencia(nova_ocorrencia)
+
         return redirect(url_for("consultar_por_id", id=novo_id))
 
     return render_template("operador.html")
 
+# =========================
+# EMAIL PARA COORDENADOR
+# =========================
+
+def enviar_email_nova_ocorrencia(ocorrencia):
+
+    email_remetente = "igorsalesleallima@gmail.com"
+    senha_app = "vdkhzrxhqppzgxvj"
+
+    # EMAIL DO COORDENADOR (VOCÊ)
+    email_destino = "igorsalesleallima@gmail.com"
+
+    mensagem = f"""
+Nova ocorrência registrada no sistema
+
+ID: {ocorrencia['id']}
+Setor: {ocorrencia['setor']}
+
+Operador: {ocorrencia['operador']}
+Email: {ocorrencia['email']}
+
+Descrição:
+{ocorrencia['descricao']}
+
+Status: {ocorrencia['status']}
+"""
+
+    msg = MIMEText(mensagem)
+
+    msg["Subject"] = f"Nova Ocorrência #{ocorrencia['id']}"
+    msg["From"] = email_remetente
+    msg["To"] = email_destino
+
+    try:
+
+        servidor = smtplib.SMTP("smtp.gmail.com", 587)
+        servidor.starttls()
+
+        servidor.login(email_remetente, senha_app)
+
+        servidor.sendmail(
+            email_remetente,
+            email_destino,
+            msg.as_string()
+        )
+
+        servidor.quit()
+
+        print("Email enviado para coordenador")
+
+    except Exception as erro:
+
+        print("Erro ao enviar email:", erro)
+        
+# =========================
+# EMAIL PARA USUÁRIO
+# =========================
+
+def enviar_email_usuario(ocorrencia):
+
+    email_remetente = "igorsalesleallima@gmail.com"
+    senha_app = "vdkhzrxhqppzgxvj"
+
+    email_destino = ocorrencia["email"]
+
+    mensagem = f"""
+Sua ocorrência foi resolvida
+
+ID: {ocorrencia['id']}
+Setor: {ocorrencia['setor']}
+
+Técnico: {ocorrencia['tecnico']}
+
+Ação realizada:
+{ocorrencia['acao']}
+
+Status: {ocorrencia['status']}
+"""
+
+    msg = MIMEText(mensagem)
+
+    msg["Subject"] = f"Ocorrência Resolvida #{ocorrencia['id']}"
+    msg["From"] = email_remetente
+    msg["To"] = email_destino
+
+    try:
+
+        servidor = smtplib.SMTP("smtp.gmail.com", 587)
+        servidor.starttls()
+
+        servidor.login(email_remetente, senha_app)
+
+        servidor.sendmail(
+            email_remetente,
+            email_destino,
+            msg.as_string()
+        )
+
+        servidor.quit()
+
+        print("Email enviado para usuário")
+
+    except Exception as erro:
+
+        print("Erro ao enviar email:", erro)
+
 
 # =========================
-# B) MÓDULO COORDENADOR
+# GERIR OCORRÊNCIA
 # =========================
 
 @app.route("/gerir", methods=["GET", "POST"])
@@ -114,6 +318,7 @@ def gerir_ocorrencia():
         id_busca = request.form.get("id")
 
         for o in dados:
+
             if o["id"] == id_busca:
 
                 ocorrencia = o
@@ -124,11 +329,15 @@ def gerir_ocorrencia():
                     acao = request.form.get("acao")
 
                     if tecnico and acao:
+
                         o["tecnico"] = tecnico.strip()
                         o["acao"] = acao.strip()
                         o["status"] = "Fechado"
 
                         salvar_ocorrencias(dados)
+                        
+                        enviar_email_usuario(o)
+
 
                 break
 
@@ -136,7 +345,7 @@ def gerir_ocorrencia():
 
 
 # =========================
-# C) CONSULTA PÚBLICA
+# CONSULTA
 # =========================
 
 @app.route("/consultar", methods=["GET", "POST"])
@@ -150,6 +359,7 @@ def consultar_ocorrencia():
         dados = carregar_ocorrencias()
 
         for o in dados:
+
             if o["id"] == id_busca:
                 ocorrencia = o
                 break
@@ -163,6 +373,7 @@ def consultar_por_id(id):
     dados = carregar_ocorrencias()
 
     for o in dados:
+
         if o["id"] == id:
             return render_template("consulta.html", ocorrencia=o)
 
